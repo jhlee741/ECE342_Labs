954c15ad1e8ca86a352271f30a608307cc1887de
John Lee <leejoh26@WIN.ecf.utoronto.ca>
2024-01-25 12:49:00 -0500

    completed lab 2

--- lab2/Core/Src/main.c
+++ lab2/Core/Src/main.c
@@ -20,6 +20,7 @@
 #include <stdio.h>
 #include <math.h>
 
+#define PI 3.14
 
 int main(void)
 {
@@ -36,8 +37,11 @@ int main(void)
   MX_USB_OTG_FS_PCD_Init();
   MX_DAC_Init();
 
-  uint16_t adc_res, mask = 0xff00;
-  char message[100];
+  uint16_t adc_res, dac_out, mask = 0xff00; //c=1100
+	uint16_t freqs[5]={10000, 0,0,0,0};
+	uint16_t sin_outs[5]={0,0,0,0,0};
+  char message[100]="Hello";
+	print_msg(message);
   
   //  ADC example
   HAL_ADC_Start(&hadc3);
@@ -50,10 +54,34 @@ int main(void)
   HAL_DAC_Init(&hdac);
   HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
   HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, 255);
-
+	
+	uint32_t start_time=HAL_GetTick();
+	float step=PI/freqs[0];
+	float arg=0;
   while (1)
   {
-    HAL_GPIO_TogglePin(LD2_GPIO_Port, LD2_Pin);
-    HAL_Delay(500);
+		/* //Part 1,2,3
+		HAL_ADC_Start(&hadc3);
+		HAL_ADC_PollForConversion(&hadc3, 10);
+    adc_res = HAL_ADC_GetValue(&hadc3);
+		adc_res=adc_res&mask;
+		HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+		HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, adc_res);
+		*/
+		
+		//part 4
+		//float arg=(HAL_GetTick()-start_time)*freqs[0]/1000;
+		
+		arg+=step;
+		/*
+		while (arg>2*PI){
+			arg-=2*PI;
+		}*/
+		
+		dac_out=2000*sin(arg);
+		HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+		HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, dac_out);
+		//sprintf(message, "dacout=%f\r\n",arg);
+		//print_msg(message);
   }
  }
