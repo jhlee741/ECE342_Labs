dd2275ff2f4e39a08cecf7af7993a8c7eb54c5e9
John Lee <jhlee90741@gmail.com>
2024-02-02 13:19:11 -0500

    Update main.c

--- lab3-kit/Core/Src/main.c
+++ lab3-kit/Core/Src/main.c
@@ -122,3 +122,136 @@ int main(void)
   }
 	//free(sinLUT);
 }
+
+/*
+
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+#include "main.h"
+#include "config.h"
+#include "fixedpt.h"
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+      const double pi = 3.14159265358979323846;
+ 
+ 
+      fixedpt lookup[628]; 
+
+
+int main(void)
+{
+  /* Reset of all peripherals. */
+  HAL_Init();
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART3_UART_Init();
+  MX_USB_OTG_FS_PCD_Init();
+  MX_DAC_Init();
+      
+      // INITIALIZE THE CLOCK
+      HAL_TIM_Base_Start();
+
+  uint16_t adc_res, mask = 0xff00;
+
+  char message[100];
+      // float *lookup = (float *)malloc(628 * sizeof(float));
+      double frequency = 0.01;
+ 
+  //  ADC example
+      int iterator = 0;
+  HAL_DAC_Init(&hdac);
+      HAL_DAC_Start(&hdac, DAC_CHANNEL_1);
+      
+      float start = 0;
+      
+      for(int i = 0; i < 628; i++){
+            lookup[i] = FXD_FROM_FLOAT(sin(start));
+            start += frequency;
+      }     
+      
+      
+       
+      int timerValPrev = __HAL_TIM_GET_COUNTER(); 
+
+ while (1) {
+       
+                        float temp = 0;
+
+                        if(iterator > 628) {
+                              break; 
+                        } else {
+                              temp = (float) (lookup[iterator % 628]+5)*0.5* pow(2,10); // -> will generate perfect sine wave 
+                        }
+                        
+       
+                  
+                  /*    for(int harmonic = 1; harmonic < 200; harmonic += 2){
+                              temp += (float) (lookup[(harmonic * iterator) % 628]+5)* 4/(pi * harmonic) * pow(2,10); // will generate square wave 
+                        } */
+                  
+       
+                  /*
+                  for(int harmonic = 1; harmonic < 50; harmo+nic++){
+                              temp += (float) (lookup[(harmonic * iterator) % 628]+ 4)* 1/(harmonic) * pow(2,9); //will generate sawtooth
+                  }
+                  */
+                        
+                  /* for(int harmonic = 1; harmonic < 100; harmonic += 2){
+                              temp += (float) (lookup[(harmonic * iterator) % 628] + 2)* pow(-1, ((harmonic - 1)/2))/pow(harmonic, 2) * pow(2, 9); // will generate triangular wave
+                  } */
+             
+             
+                  ////GENERATES THE FIXED POINT SQUARE WAVE 
+                  /*    
+                   fixedpt tempVal = FXD_FROM_FLOAT(0.5); 
+                   
+                   for(int harmonic = 0; harmonic < 20; harmonic++) {
+                         
+                              fixedpt harmonicDivision = FXD_DIV(FXD_FROM_FLOAT(2.0), FXD_FROM_FLOAT(pi));              
+                         // take 2/pi
+
+                          fixedpt harmonicDivision2 = FXD_DIV(FXD_FROM_FLOAT(1.0), FXD_FROM_INT(harmonic*2+1));                                 
+                         // 1/n 
+
+                          fixedpt lookupValue = FXD_MUL(FXD_FROM_FLOAT(lookup[(((harmonic*2+1) * iterator) % 628)]), harmonicDivision); 
+                          fixedpt lookupValue2 = FXD_MUL(lookupValue, harmonicDivision2); 
+                         // take the sinusoid value and cast it to a fixed point type 
+
+                         tempVal += FXD_ADD(tempVal, lookupValue2); 
+                  } 
+                   
+                  temp = (2000 * FXD_TO_FLOAT(tempVal)) + 1000; 
+                  temp *= pow(2, 9); 
+                  */
+                  
+                  HAL_DAC_SetValue(&hdac, DAC_CHANNEL_1, DAC_ALIGN_12B_R, temp);
+                  iterator++; 
+                  
+      }
+ 
+      int timerValFinal = __HAL_TIM_GET_COUNTER(); 
+      
+      sprintf(message, "%d", timerValFinal - timerValPrev);
+      print_msg(message);
+}
+*/
